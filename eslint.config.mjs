d2c96ae46276d83756791a77175f0a66debe9cdb
asif419 <asshah419@gmail.com>
2025-01-20 20:08:14 +0200

    installing all dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,33 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
     rules: {
-      eqeqeq: "off",
-      "@typescript-eslint/no-unused-vars": "off",
+      eqeqeq: 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
       // 'no-unused-vars': 'error',
-      "no-unused-expressions": "error",
-      "no-console": "warn",
-      "no-undef": "error",
-      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      'no-unused-expressions': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
     },
   },
   {
-    ignores: [".node_modules/*", "dist"],
+    ignores: ['.node_modules/*', 'dist'],
   },
 ];
